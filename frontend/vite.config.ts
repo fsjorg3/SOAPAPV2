5b1cdd6b56371a8b2eeb66a4ffbe470a585d399b
Jorge F.S. <f.s.jorg3@gmail.com>
2026-06-08 18:23:34 -0600

    agregabndo base de ruta

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,9 @@ import babel from '@rolldown/plugin-babel'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+
     react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+    babel({ presets: [reactCompilerPreset()] }),
+
+  ], base: '/',
 })
